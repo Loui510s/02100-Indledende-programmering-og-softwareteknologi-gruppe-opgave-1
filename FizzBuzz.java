ee18a932bfd370c8afce2e22b1df04f655ec3fdb
Louie Olsen <Louie.Ferdinand05@gmail.com>
2025-09-19 15:14:47 +0200

    Fixed FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,12 @@
+//Der bliver lavet en for løkke og 3 modulus operationer for at give resultatet Fizz, Buzz og FizzBuzz  
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        for (int i = 1; i <= 30; i++) {
+            String result = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                            (i % 3 == 0) ? "Fizz" :
+                            (i % 5 == 0) ? "Buzz" :
+                            Integer.toString(i);
+            System.out.println(result);
         }
     }
 }
